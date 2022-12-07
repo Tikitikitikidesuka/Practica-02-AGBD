0c0abd849ae2a8a1089d9b0fb2a45058206bd785
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-12-07 21:09:25 +0100

    Añadir script de creción de usuarios y roles de ejemplo

---
+++ scripts/temp_user_and_role_creation.sql
@@ -0,0 +1,13 @@
+CREATE ROLE 'testo_role'@'%';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON PracABD1.* TO 'testo_role'@'%';
+
+CREATE USER 'testo_user'@'%' IDENTIFIED BY 'Keo';
+
+GRANT 'testo_role'@'%' TO 'testo_user'@'%';
+
+SELECT current_role();
+
+SET DEFAULT ROLE ALL TO 'testo_user'@'%';  # IMPORTANTE B)
+
+SELECT current_role();

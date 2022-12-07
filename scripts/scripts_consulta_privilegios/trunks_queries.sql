4b415040dac3475f235627c155134c384ba4f995
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-12-08 00:53:34 +0100

    Añadir scripts de test por queries de Trunks y Goku

---
+++ scripts/scripts_consulta_privilegios/trunks_queries.sql
@@ -0,0 +1,9 @@
+#INICIADO SESIÓN COMO TRUNKS
+
+# VISUALIZAR
+SELECT * FROM clientes;
+
+
+# INDICAR ALQUILER
+INSERT INTO `clientes_juegos` (`ClienteID`, `JuegoID`, `FechaAlquiler`, `Comentarios`)
+VALUES (1, 12001, 'DATE: Auto CURDATE()', 'XD paraparapapappappppowwn :)')

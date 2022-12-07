401bb3cc798d281ae70f9a9c01e80b94c8a9416f
Alex Fernandez <alex29561@gmail.com>
2022-12-08 00:42:27 +0100

    Modificar esquema del repositorio

---
+++ scripts/scripts_consulta_privilegios/gohan_queries.sql
@@ -0,0 +1,27 @@
+#INICIADO SESIÓN COMO GOHAN
+
+#SENTENCIAS CON PERMISOS
+SET SQL_SAFE_UPDATES = 0;
+
+SELECT COUNT(*) AS Ventas, juegos.Titulo
+	FROM clientes_juegos JOIN juegos ON clientes_juegos.juegoID = juegos.juegoID
+	GROUP BY juegos.juegoID, juegos.Titulo
+	HAVING COUNT(*) >= ALL (SELECT COUNT(*)
+								FROM clientes_juegos
+								GROUP BY juegoID
+							);
+                            
+INSERT INTO `juegos` (`JuegoID`, `Titulo`, `Consola`, `Tamanio`, `Editor`)
+VALUES (10000, "Cowabunga", "MegaDrive", 32768, "UPMGames");
+
+SET SQL_SAFE_UPDATES = 1;
+
+#SENTENCIAS SIN PERMISOS
+DROP TABLE juegos;
+
+CREATE TABLE juegosDeMesa (
+	JuegoID INT UNIQUE NOT NULL,
+	Titulo VARCHAR(256) /*UNIQUE*/ NOT NULL,
+	NumeroJugadores INT,
+	Empresa VARCHAR(32)
+)
